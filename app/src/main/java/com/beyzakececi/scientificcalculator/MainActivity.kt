4c8449177985b802c1bb458ab130ebaac790bfff
beyzakececi <beyzakececi.35@gmail.com>
2023-07-26 19:42:29 +0300

    Add MathParser.org-mXparser-5.2.1.jar and Complete calculator

--- app/src/main/java/com/beyzakececi/scientificcalculator/MainActivity.kt
+++ app/src/main/java/com/beyzakececi/scientificcalculator/MainActivity.kt
@@ -3,21 +3,20 @@ package com.beyzakececi.scientificcalculator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.beyzakececi.scientificcalculator.databinding.ActivityMainBinding
+import org.mariuszgromada.math.mxparser.Expression
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //create view binding
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        // set show soft input on focus is false
-        //binding.displayEditText?.showSoftInputOnFocus(false)
+        binding.displayEditText?.showSoftInputOnFocus = false
 
 
         binding.zero.setOnClickListener { onNumberButtonClick("0") }
@@ -38,6 +37,22 @@ class MainActivity : AppCompatActivity() {
         binding.multiplyButton.setOnClickListener { onNumberButtonClick("*") }
         binding.divideButton.setOnClickListener { onNumberButtonClick("/") }
 
+        binding.sinButton?.setOnClickListener { onNumberButtonClick("sin(") }
+        binding.cosButton?.setOnClickListener { onNumberButtonClick("cos(") }
+        binding.tanButton?.setOnClickListener { onNumberButtonClick("tan(") }
+        binding.arcsin?.setOnClickListener { onNumberButtonClick("arcsin(") }
+        binding.arccos?.setOnClickListener { onNumberButtonClick("arccos(") }
+        binding.arctan?.setOnClickListener { onNumberButtonClick("arctan(") }
+        binding.logButton?.setOnClickListener { onNumberButtonClick("log(") }
+        binding.lnButton?.setOnClickListener { onNumberButtonClick("ln(") }
+        binding.absolute?.setOnClickListener { onNumberButtonClick("abs(") }
+        binding.pi?.setOnClickListener { onNumberButtonClick("pi") }
+        binding.eButton?.setOnClickListener { onNumberButtonClick("e") }
+        binding.sqrtButton?.setOnClickListener { onNumberButtonClick("^(2)") }
+        binding.xsquarey?.setOnClickListener { onNumberButtonClick("^(") }
+        binding.factoriel?.setOnClickListener { onNumberButtonClick("!") }
+
+
 
         // Set click listener for the "Clear" button
         binding.clearButton.setOnClickListener { onClearButtonClick() }
@@ -49,16 +64,27 @@ class MainActivity : AppCompatActivity() {
         binding.backspaceButton.setOnClickListener { onBackspaceButtonClick() }
 
 
+    }
 
-
-
-
+    private fun updateText(strToAdd: String) {
+        val oldStr = binding.displayEditText?.text.toString()
+        val cursorPos = binding.displayEditText?.selectionStart
+        val leftStr = cursorPos?.let { oldStr.substring(0, it) }
+        val rightStr = cursorPos?.let { oldStr.substring(it) }
+        binding.displayEditText?.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr))
+        binding.displayEditText?.setSelection(cursorPos?.plus(strToAdd.length) ?: 0)
     }
 
     private fun onBackspaceButtonClick() {
-        val text = binding.displayEditText?.text.toString()
-        if (text.isNotEmpty()) {
-            binding.displayEditText?.text?.delete(text.length - 1, text.length)
+        val cursorPos = binding.displayEditText?.selectionStart
+        val textLen = binding.displayEditText?.text?.length
+        if (cursorPos != null && textLen != null) {
+            if (cursorPos != 0 && textLen != 0) {
+                val selection = cursorPos - 1
+                val text = binding.displayEditText?.text
+                text?.delete(selection, cursorPos)
+                binding.displayEditText?.setSelection(selection)
+            }
         }
     }
 
@@ -69,12 +95,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onEqualButtonClick() {
-        val text = binding.displayEditText?.text.toString()
+        var userExp = binding.displayEditText?.text.toString()
+        binding.previousCalculationView?.text= userExp
+        userExp = userExp.replace(resources.getString(R.string.divideText), "/")
+        userExp = userExp.replace(resources.getString (R.string.multiplyText), "*")
+        val exp = Expression(userExp)
+        val result = exp.calculate().toString()
+        binding.displayEditText?.setText(result)
 
     }
 
     private fun onClearButtonClick() {
         binding.displayEditText?.text?.clear()
+        binding.previousCalculationView.text = ""
     }
 
     private fun onNumberButtonClick(s: String) {
@@ -83,3 +116,5 @@ class MainActivity : AppCompatActivity() {
 
 
 }
+
+
